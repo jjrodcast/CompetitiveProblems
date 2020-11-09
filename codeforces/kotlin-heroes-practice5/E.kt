0bead7878550ef3a1c345d694f0d47e1d5454476
Jorge Rodríguez <jjorge.rc93@gmail.com>
2020-11-09 14:45:33 -0500

    Kotlin Heroes Problem E solved

--- codeforces/kotlin-heroes-practice5/E.kt
+++ codeforces/kotlin-heroes-practice5/E.kt
@@ -1,5 +1,3 @@
-import java.util.*
-
 private fun readLn() = readLine()!!
 private fun readInt() = readLn().toInt()
 private fun readLong() = readLn().toLong()
@@ -7,37 +5,28 @@ private fun readStrings() = readLn().split(" ")
 private fun readInts() = readStrings().map { it.toInt() }
 private fun readLongs() = readStrings().map { it.toLong() }
 
-// This solution is giving TLE (refactor and test it again with a beeter approach)
-private fun minimizing(k: Int, binary: CharArray): String {
-    var movements = k
-    val queue: Queue<Int> = LinkedList()
-    var sol = ""
-    for ((idx, c) in binary.withIndex()) {
-        if (c == '0') queue.add(idx)
-    }
-    binary.forEachIndexed { idx, _ ->
-        if (queue.isNotEmpty()) {
-            val zeroPos = queue.peek()!!
-            val movementsNeeded = zeroPos - idx
-            if (movementsNeeded <= movements) {
-                movements -= movementsNeeded
-                sol += '0'
-                queue.remove()
-            } else {
-                sol += '1'
-            }
-        } else {
-            sol += '1'
+private fun minimizingLinear(n: Int, k: Long, binary: CharArray): String {
+    val sol = CharArray(n) { '1' }
+    var moveTo = 0
+    var totalMovements = k
+    for (currentPos in binary.indices) {
+        if (binary[currentPos] == '0') {
+            val canMove = minOf(totalMovements, (currentPos - moveTo).toLong())
+            totalMovements -= canMove
+            val pos = (currentPos - canMove).toInt()
+            sol[pos] = '0'
+            moveTo++
         }
     }
-    return sol
+
+    return sol.joinToString(separator = "")
 }
 
 fun main() {
     val q = readInt()
     repeat(q) {
-        val (n, k) = readInts()
+        val (n, k) = readLongs()
         val binary = readLn()
-        println(minimizing(k, binary.toCharArray()))
+        println(minimizingLinear(n.toInt(), k, binary.toCharArray()))
     }
 }
